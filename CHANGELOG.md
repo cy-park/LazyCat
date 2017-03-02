20e09eff493e3f2b07bd610610b112b8a1fd1238
Chan Young Park <chpark@ml-t0001249.ngs.org>
2017-03-02 16:51:46 -0500

    Initial commit
    ## 0.1.0 (2017-03-02, beta release)
    
    #### features:
     - added bolierplate files
     - added LazyCat.Image()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,13 @@
 CHANGELOG
 =========
 
-## 0.0.1 (Unreleased)
+## 0.1.1 (Unreleased)
+
+## 0.1.0 (2017-03-02, beta release)
 
 #### features:
  - added bolierplate files
+ - added LazyCat.Image()
 
 #### updates:
 
--- src/LazyCat.js
+++ src/LazyCat.js
@@ -4,7 +4,40 @@
 
 'use strict';
 
-var LazyCat = function(args){};
+var LazyCat = {};
+
+LazyCat.Image = function(selector, callback, callbackArgs, callbackError, callbackArgsError){
+
+	var elms = document.querySelectorAll(selector);
+
+	for (var i = 0; i < elms.length; i++) {
+		(function(_i){
+			var img = new Image();
+
+			img.src = elms[_i].getAttribute('data-lazycat-image');
+
+			img.onload = function(){
+
+				if (elms[_i].tagName.toLowerCase() === 'img') {
+					elms[_i].setAttribute('src',img.src);
+				} else {
+					var style_text = 'background-image:url("'+img.src+'");'+
+									 elms[_i].getAttribute('style');
+
+					elms[_i].setAttribute('style',style_text);
+				}
+
+				if (callback)
+					callback.apply(elms[_i], callbackArgs);
+			};
+
+			img.onerror = function(){
+				if (callbackError)
+					callbackError.apply(elms[_i], callbackArgsError);
+			};
+		}(i));
+	}
+};
 
 if (typeof define === 'function' && define.amd) define(LazyCat);
 else if (typeof module === 'object' && module.exports) module.exports = LazyCat;
